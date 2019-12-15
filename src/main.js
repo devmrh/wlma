59dc9455e700f7e70e14aa7dfa4c4f8dcac993b0
devmrh <dev.mohammadreza@yahoo.com>
2019-12-15 23:28:53 +0330

    set header for axios

--- src/main.js
+++ src/main.js
@@ -1,16 +1,12 @@
 import App from './App.vue'
 import Vue from 'vue'
-import  axios  from 'axios'
-window.axios = axios;
+window.axios = require('axios');
 
 Vue.config.productionTip = false
 
-
-//let token = localStorage.get("token")
-//if(token){
-Vue.axios.headers.common["X-ZUMO-AUTH"] =
-    "eyJhbGciOiJodHRwOi8vd3d3LnczLm9yZy8yMDAxLzA0L3htbGRzaWctbW9yZSNobWFjLXNoYTI1NiIsInR5cCI6IkpXVCJ9.eyJuYmYiOjE1NzY0MjkyMjUsImV4cCI6MTU4NDIwNTIyNiwiaXNzIjoiQU1TIiwiYXVkIjoiVGVzdEFwaSIsInN1YiI6IjEyNiIsIkFwcElkIjotMX0.3SQpsHt2pmsuXBzUIMJUgBIPYux2IVD-fxeeAGhdz2k";
-//}
+// Set token in requests header
+// we should check if token is saved in browser later
+window.axios.defaults.headers.common["X-ZUMO-AUTH"] = "eyJhbGciOiJodHRwOi8vd3d3LnczLm9yZy8yMDAxLzA0L3htbGRzaWctbW9yZSNobWFjLXNoYTI1NiIsInR5cCI6IkpXVCJ9.eyJuYmYiOjE1NzY0MjkyMjUsImV4cCI6MTU4NDIwNTIyNiwiaXNzIjoiQU1TIiwiYXVkIjoiVGVzdEFwaSIsInN1YiI6IjEyNiIsIkFwcElkIjotMX0.3SQpsHt2pmsuXBzUIMJUgBIPYux2IVD-fxeeAGhdz2k";
 
 new Vue({
   render: h => h(App),
